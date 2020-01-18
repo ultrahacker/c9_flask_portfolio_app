e3bf244fb2b36ad2649826f65a39dd36adcc8b39
ultrahacker <jimchi.ucf@gmail.com>
2020-01-17 19:33:15 -0500

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def home_page():
 
 @app.route('/<name>')
 def profile(name):
-	new_name = str(name) + "Need some help"
+	new_name = str(name) + "need some help"
 	return render_template('index.html', name=new_name)
 
 
